b6989b569e018228342897cbabdb89a1dc88d02d
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-12-01 18:48:02 +0100

    🔊 : add time logging

--- src/main/kotlin/com/github/juwit/adventofcode2020/Main.kt
+++ src/main/kotlin/com/github/juwit/adventofcode2020/Main.kt
@@ -1,6 +1,7 @@
 package com.github.juwit.adventofcode2020
 
 import com.github.juwit.adventofcode2020.day01.Day1
+import kotlin.system.measureTimeMillis
 
 fun main() {
     println("[AdventOfCode - 2020]")
@@ -14,11 +15,19 @@ abstract class Day(val id: Int, val title: String){
 
     fun solveDay() {
         println("--- Day ${this.id}: ${this.title} ---")
-        val part1Solution = this.solvePart1()
-        println("Puzzle answer : $part1Solution")
-        val part2Solution = this.solvePart2()
+        var part1Solution: String
+        val part1Time = measureTimeMillis {
+            part1Solution = this.solvePart1()
+        }
+        println("Puzzle answer : $part1Solution - time : $part1Time ms")
+
         println("--- Part Two ---")
-        println("Puzzle answer : $part2Solution")
+        var part2Solution: String
+        val part2Time = measureTimeMillis {
+            part2Solution = this.solvePart2()
+        }
+        println("Puzzle answer : $part2Solution - time $part2Time ms")
+
         println()
     }
 
