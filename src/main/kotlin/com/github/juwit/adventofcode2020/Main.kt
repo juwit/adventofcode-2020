008a159f6ab9bf00f24cc407aaa0cb210ee5ddb3
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-12-01 18:42:27 +0100

    ♻️ : extract Day abstract class

--- src/main/kotlin/com/github/juwit/adventofcode2020/Main.kt
+++ src/main/kotlin/com/github/juwit/adventofcode2020/Main.kt
@@ -1,9 +1,28 @@
-import com.github.juwit.adventofcode2020.day01.solve_part1
-import com.github.juwit.adventofcode2020.day01.solve_part2
+package com.github.juwit.adventofcode2020
+
+import com.github.juwit.adventofcode2020.day01.Day1
 
 fun main() {
-    println("adventofcode - 2020")
+    println("[AdventOfCode - 2020]")
+    println()
 
-    solve_part1()
-    solve_part2()
+    val day1 = Day1()
+    day1.solveDay()
+}
+
+abstract class Day(val id: Int, val title: String){
+
+    fun solveDay() {
+        println("--- Day ${this.id}: ${this.title} ---")
+        val part1Solution = this.solvePart1()
+        println("Puzzle answer : $part1Solution")
+        val part2Solution = this.solvePart2()
+        println("--- Part Two ---")
+        println("Puzzle answer : $part2Solution")
+        println()
+    }
+
+    abstract fun solvePart1(): String
+
+    abstract fun solvePart2(): String
 }
--- src/main/kotlin/com/github/juwit/adventofcode2020/day01/Day1.kt
+++ src/main/kotlin/com/github/juwit/adventofcode2020/day01/Day1.kt
@@ -1,34 +1,37 @@
 package com.github.juwit.adventofcode2020.day01
 
-val input = "/input-day-01.txt".asIntList()
+import com.github.juwit.adventofcode2020.Day
 
-fun solve_part1() {
-    println("--- Day 1: Report Repair ---")
+class Day1: Day(1, "Report Repair"){
 
     val input = "/input-day-01.txt".asIntList()
 
-    for (a in input) {
-        for(b in input) {
-            if(a+b == 2020){
-                println("Puzzle answer : ${a*b}");
-                return
-            }
-        }
-    }
-}
-
-fun solve_part2() {
-    println("--- Part Two ---");
-    for (a in input) {
-        for(b in input) {
-            for(c in input) {
-                if(a+b+c == 2020){
-                    println("Puzzle answer : ${a*b*c}");
-                    return
+    override fun solvePart1(): String {
+        for (a in input) {
+            for(b in input) {
+                if(a+b == 2020){
+                    return (a*b).toString()
                 }
             }
         }
+
+        throw IllegalStateException("Cloud not determine puzzle answer")
     }
+
+    override fun solvePart2(): String {
+        for (a in input) {
+            for(b in input) {
+                for(c in input) {
+                    if(a+b+c == 2020){
+                        return (a*b*c).toString()
+                    }
+                }
+            }
+        }
+
+        throw IllegalStateException("Cloud not determine puzzle answer")
+    }
+
 }
 
 fun String.asString(): String = {}.javaClass.getResource(this).readText()
