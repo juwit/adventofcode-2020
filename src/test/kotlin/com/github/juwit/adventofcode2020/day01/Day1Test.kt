6d5ab1928d532573e615887d2115bcc4ca7e84d7
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-12-01 20:04:18 +0100

    ✅ : add tests for day 1

---
+++ src/test/kotlin/com/github/juwit/adventofcode2020/day01/Day1Test.kt
@@ -0,0 +1,32 @@
+package com.github.juwit.adventofcode2020.day01
+
+import com.github.juwit.adventofcode2020.asStringList
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class Day1Test{
+
+    val testInput = """
+        1721
+        979
+        366
+        299
+        675
+        1456
+    """.trimIndent().asStringList()
+
+    @Test
+    fun part1_should_find_matching_entries(){
+        val day1 = Day1()
+
+        assertEquals("514579", day1.solvePart1(testInput))
+    }
+
+    @Test
+    fun part2_should_find_matching_entries(){
+        val day1 = Day1()
+
+        assertEquals("241861950", day1.solvePart2(testInput))
+    }
+
+}
