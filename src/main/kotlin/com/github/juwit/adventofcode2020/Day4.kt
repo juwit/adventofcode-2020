ea4ab26febcda379fb844bc632162a7175ed7402
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-12-04 10:18:43 +0100

    ✨ day 4 - part 2

--- src/main/kotlin/com/github/juwit/adventofcode2020/Day4.kt
+++ src/main/kotlin/com/github/juwit/adventofcode2020/Day4.kt
@@ -8,7 +8,6 @@ data class Passport(val map: Map<String, String>) {
     val hcl by map
     val ecl by map
     val pid by map
-    val cid by map
 
     // to be valid, all fields MUST have a value, except "cid"
     fun hasAllRequiredFields() = byr.isNotBlank()
@@ -18,7 +17,43 @@ data class Passport(val map: Map<String, String>) {
             && hcl.isNotBlank()
             && ecl.isNotBlank()
             && pid.isNotBlank()
+
+    fun isValid() = hasAllRequiredFields()
+            && byr.isBYRValid()
+            && iyr.isIYRValid()
+            && eyr.isEYRValid()
+            && hgt.isHGTValid()
+            && hcl.isHCLValid()
+            && ecl.isECLValid()
+            && pid.isPIDValid()
 }
+
+fun String.isBYRValid(): Boolean = this.toInt() in 1920..2002
+
+fun String.isIYRValid(): Boolean = this.toInt() in 2010..2020
+
+fun String.isEYRValid(): Boolean = this.toInt() in 2020..2030
+
+fun String.isHGTValid(): Boolean {
+    val ranges = mapOf(
+        "in" to 59..76,
+        "cm" to 150..193
+    )
+    val regex = Regex("(\\d*)(in|cm)")
+
+    if (!regex.matches(this)) {
+        return false
+    }
+
+    val (value, unit) = regex.find(this)!!.destructured
+    return value.toInt() in ranges[unit]!!
+}
+
+fun String.isHCLValid(): Boolean = Regex("#[0-9a-f]{6}").matches(this)
+
+fun String.isECLValid(): Boolean = listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(this)
+
+fun String.isPIDValid(): Boolean = Regex("\\d{9}").matches(this)
 
 fun parsePassportLine(line: String): Map<String, String> {
     // parse the line using magic regex
@@ -57,6 +92,6 @@ class Day4 : Day(4, "Passport Processing") {
     }
 
     override fun solvePart2(input: List<String>): String {
-        TODO("Not yet implemented")
+        return parsePassports(input).count { it.isValid() }.toString()
     }
 }
--- src/test/kotlin/com/github/juwit/adventofcode2020/Day4Test.kt
+++ src/test/kotlin/com/github/juwit/adventofcode2020/Day4Test.kt
@@ -1,12 +1,12 @@
 package com.github.juwit.adventofcode2020
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class Day4Test {
 
-    val testInput = """
+    private val testInput = """
         ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
         byr:1937 iyr:2017 cid:147 hgt:183cm
         
@@ -20,6 +20,37 @@ internal class Day4Test {
         
         hcl:#cfa07d eyr:2025 pid:166559648
         iyr:2011 ecl:brn hgt:59in
+    """.trimIndent().asStringList()
+
+    private val invalidPassports = """
+        eyr:1972 cid:100
+        hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+        iyr:2019
+        hcl:#602927 eyr:1967 hgt:170cm
+        ecl:grn pid:012533040 byr:1946
+
+        hcl:dab227 iyr:2012
+        ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+        hgt:59cm ecl:zzz
+        eyr:2038 hcl:74454a iyr:2023
+        pid:3556412378 byr:2007
+    """.trimIndent().asStringList()
+
+    private val validPassports = """
+        pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+
+        eyr:2029 ecl:blu cid:129 byr:1989
+        iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+        hcl:#888785
+        hgt:164cm byr:2001 iyr:2015 cid:88
+        pid:545766238 ecl:hzl
+        eyr:2022
+
+        iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
     """.trimIndent().asStringList()
 
     @Test
@@ -50,10 +81,58 @@ internal class Day4Test {
     }
 
     @Test
-    fun part2_should_count_valid_passports() {
-        val day = Day4()
+    fun test_byr_validation() {
+        assertTrue("2002".isBYRValid())
+        assertFalse("2003".isBYRValid())
+    }
 
-        TODO("Not yet implemented")
+    @Test
+    fun test_iyr_validation() {
+        assertFalse("2009".isIYRValid())
+        assertTrue("2020".isIYRValid())
+    }
+
+    @Test
+    fun test_eyr_validation() {
+        assertFalse("2019".isEYRValid())
+        assertTrue("2025".isEYRValid())
+    }
+
+    @Test
+    fun test_hgt_validation() {
+        assertTrue("60in".isHGTValid())
+        assertTrue("190cm".isHGTValid())
+        assertFalse("190in".isHGTValid())
+        assertFalse("190".isHGTValid())
+    }
+
+    @Test
+    fun test_hcl_validation() {
+        assertTrue("#123abc".isHCLValid())
+        assertFalse("#123abz".isHCLValid())
+        assertFalse("123abc".isHCLValid())
+    }
+
+    @Test
+    fun test_ecl_validation() {
+        assertTrue("brn".isECLValid())
+        assertFalse("wat".isECLValid())
+    }
+
+    @Test
+    fun test_pid_validation() {
+        assertTrue("000000001".isPIDValid())
+        assertFalse("0123456789".isPIDValid())
+    }
+
+    @Test
+    fun part2_should_recognize_invalid_passports() {
+        assertTrue(parsePassports(invalidPassports).all { !it.isValid() })
+    }
+
+    @Test
+    fun part2_should_recognize_valid_passports() {
+        assertTrue(parsePassports(validPassports).all { it.isValid() })
     }
 
 }
