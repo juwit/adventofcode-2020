ca88755d0a8dc7d6dba322ac898fe86146deee94
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-12-12 15:44:07 +0100

    ✨ : day 12 - part 2

--- src/main/kotlin/com/github/juwit/adventofcode2020/Day12.kt
+++ src/main/kotlin/com/github/juwit/adventofcode2020/Day12.kt
@@ -2,7 +2,34 @@ package com.github.juwit.adventofcode2020
 
 import kotlin.math.abs
 
-data class Ship(var north: Int = 0, var east: Int = 0, var orientation: Int = 0)
+data class Ship(var north: Int = 0, var east: Int = 0, var orientation: Int = 0) {
+    fun moveTo(waypoint: Waypoint, value: Int) {
+        this.north += waypoint.north * value
+        this.east += waypoint.east * value
+    }
+}
+
+data class Waypoint(var north: Int = 1, var east: Int = 10) {
+    fun rotateLeft(degrees: Int): Waypoint {
+        for(i in 0..(degrees/90)-1){
+            val newNorth = east
+            val newEast = -north
+            north = newNorth
+            east = newEast
+        }
+        return this
+    }
+
+    fun rotateRight(degrees: Int): Waypoint {
+        for(i in 0..(degrees/90)-1){
+            val newNorth = -east
+            val newEast = north
+            north = newNorth
+            east = newEast
+        }
+        return this
+    }
+}
 
 class Day12: Day(12, "Rain Risk") {
     override fun solvePart1(input: List<String>): String {
@@ -36,6 +63,24 @@ class Day12: Day(12, "Rain Risk") {
     }
 
     override fun solvePart2(input: List<String>): String {
-        TODO("Not yet implemented")
+        val ship = Ship()
+        val waypoint = Waypoint()
+
+        input.forEach {
+            val action = it[0]
+            val value = it.substring(1).toInt()
+            when(action) {
+                'N' -> waypoint.north += value
+                'S' -> waypoint.north -= value
+                'E' -> waypoint.east += value
+                'W' -> waypoint.east -= value
+                'L' -> waypoint.rotateLeft(value)
+                'R' -> waypoint.rotateRight(value)
+                'F' -> ship.moveTo(waypoint, value)
+            }
+        }
+
+        // return manhattan distance from 0/0
+        return (abs(ship.north) + abs(ship.east)).toString()
     }
 }
--- src/test/kotlin/com/github/juwit/adventofcode2020/Day12Test.kt
+++ src/test/kotlin/com/github/juwit/adventofcode2020/Day12Test.kt
@@ -17,4 +17,22 @@ class Day12Test {
     fun testSolvePart1(){
         assertThat(Day12().solvePart1(testInput)).isEqualTo("25")
     }
+
+    @Test
+    fun testWaypointRotation(){
+        assertThat(Waypoint(2,1).rotateLeft(90))
+            .isEqualTo(Waypoint(1, -2))
+        assertThat(Waypoint(-2,-1).rotateLeft(90))
+            .isEqualTo(Waypoint(-1, 2))
+
+        assertThat(Waypoint(4, 10).rotateRight(90))
+            .isEqualTo(Waypoint(-10, 4))
+        assertThat(Waypoint(2,1).rotateRight(180))
+            .isEqualTo(Waypoint(-2, -1))
+    }
+
+    @Test
+    fun testSolvePart2(){
+        assertThat(Day12().solvePart2(testInput)).isEqualTo("286")
+    }
 }
