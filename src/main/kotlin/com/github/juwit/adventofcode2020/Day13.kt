29b0165e7d122db5940874e593ff9136d6b79e83
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-12-14 18:21:45 +0100

    ✨ : day 13 - part 2

--- src/main/kotlin/com/github/juwit/adventofcode2020/Day13.kt
+++ src/main/kotlin/com/github/juwit/adventofcode2020/Day13.kt
@@ -1,5 +1,7 @@
 package com.github.juwit.adventofcode2020
 
+import java.lang.IllegalArgumentException
+
 class Day13: Day(13, "Shuttle Search") {
 
     override fun solvePart1(input: List<String>): Number {
@@ -15,7 +17,54 @@ class Day13: Day(13, "Shuttle Search") {
         return busIdWithWaitingTime.first * busIdWithWaitingTime.second
     }
 
+    fun checkIfTimestampIsRight(timestamp: Long, busSpec: String) : Boolean{
+        return busSpec.split(",")
+            .mapIndexed { index, s -> s to index }
+            .filter { it.first != "x" }
+            .map { it.first.toInt() to it.second}
+            .map { (timestamp + it.second ) % it.first }
+            .all { it.toInt() == 0 }
+    }
+
+    fun checkIfTimestampIsRight(timestamp: Long, busSpecs: List<Pair<Int,Int>>) : Boolean{
+        return busSpecs
+            .map { (timestamp + it.second ) % it.first }
+            .all { it.toInt() == 0 }
+    }
+
     override fun solvePart2(input: List<String>): Number {
-        TODO("Not yet implemented")
+        // try but brute forcing
+        // with an increment of the least-common-multiple ?
+        val busSpecs = input[1]
+            .split(",")
+            .mapIndexed { index, s -> s to index }
+            .filter { it.first != "x" }
+            .map { it.first.toInt() to it.second}
+
+        // try to find when first busses align
+
+        var timestamp = 0L // start at 0
+        var increment = 1L // start with a 1 increment
+        busSpecs.forEachIndexed { index, busSpec ->
+            val (busId) = busSpec
+
+            // try to find if sub-spec is valid
+            while( ! checkIfTimestampIsRight(timestamp, busSpecs.subList(0, index+1))){
+                timestamp += increment
+
+            }
+            // found new value that matches, find other timestamps that matches
+            increment *= busId
+        }
+
+        if(checkIfTimestampIsRight(timestamp, input[1])){
+            // found !
+            return timestamp
+        }
+        else {
+            throw Exception("could not find puzzle answer")
+        }
+
+        return timestamp
     }
 }
--- src/test/kotlin/com/github/juwit/adventofcode2020/Day13Test.kt
+++ src/test/kotlin/com/github/juwit/adventofcode2020/Day13Test.kt
@@ -15,4 +15,27 @@ class Day13Test {
         assertThat(Day13().solvePart1(testInput)).isEqualTo(295)
     }
 
+    @Test
+    fun testCheckFunction(){
+        assertThat(Day13().checkIfTimestampIsRight(1068781, testInput[1])).isTrue
+
+        assertThat(Day13().checkIfTimestampIsRight(3417, "17,x,13,19")).isTrue
+        assertThat(Day13().checkIfTimestampIsRight(754018, "67,7,59,61")).isTrue
+        assertThat(Day13().checkIfTimestampIsRight(779210, "67,x,7,59,61")).isTrue
+        assertThat(Day13().checkIfTimestampIsRight(1261476, "67,7,x,59,61")).isTrue
+        assertThat(Day13().checkIfTimestampIsRight(1202161486, "1789,37,47,1889")).isTrue
+    }
+
+    @Test
+    fun testSolvePart2(){
+        assertThat(Day13().solvePart2(testInput)).isEqualTo(1068781L)
+
+        assertThat(Day13().solvePart2(listOf("","17,x,13,19"))).isEqualTo(3417L)
+        assertThat(Day13().solvePart2(listOf("","67,7,59,61"))).isEqualTo(754018L)
+        assertThat(Day13().solvePart2(listOf("","67,x,7,59,61"))).isEqualTo(779210L)
+        assertThat(Day13().solvePart2(listOf("","67,7,x,59,61"))).isEqualTo(1261476L)
+        assertThat(Day13().solvePart2(listOf("","1789,37,47,1889"))).isEqualTo(1202161486L)
+
+    }
+
 }
